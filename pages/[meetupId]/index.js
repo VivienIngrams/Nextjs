09bb92d03a0cf219fa86abd1afba1dfdcfaae799
VivienIngrams <vivingrams@gmail.com>
2023-06-26 17:06:39 +0100

    Starting API routes

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,15 +11,32 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
 export async function getStaticProps(context) {
-
-
   const meetupId = context.params.meetupId;
-  
+
   return {
     props: {
       meetupData: {
-        image: "https://hips.hearstapps.com/hmg-prod/images/moraine-lake-and-the-valley-of-the-ten-peaks-in-the-royalty-free-image-1571062944.jpg?crop=0.653xw:1.00xh;0.251xw,0&resize=2048:*",
+        image:
+          "https://hips.hearstapps.com/hmg-prod/images/moraine-lake-and-the-valley-of-the-ten-peaks-in-the-royalty-free-image-1571062944.jpg?crop=0.653xw:1.00xh;0.251xw,0&resize=2048:*",
         title: "Meetup",
         address: "Address",
         description: "Description",
