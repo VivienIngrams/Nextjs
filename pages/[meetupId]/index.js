550899c5b61e542813d99f7d8da016f22c4b16a6
VivienIngrams <vivingrams@gmail.com>
2023-06-26 16:50:06 +0100

    getServerSideProps and getStaticProps

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,31 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image=""
+      title="Meetup"
+      address="Address"
+      description="Description"
+    />
+  );
+}
+
+export async function getStaticProps(context) {
+
+
+  const meetupId = context.params.meetupId;
+  
+  return {
+    props: {
+      meetupData: {
+        image: "https://hips.hearstapps.com/hmg-prod/images/moraine-lake-and-the-valley-of-the-ten-peaks-in-the-royalty-free-image-1571062944.jpg?crop=0.653xw:1.00xh;0.251xw,0&resize=2048:*",
+        title: "Meetup",
+        address: "Address",
+        description: "Description",
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,51 @@
-import MeetupList from '../components/meetups/MeetupList';
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'Meetup 1',
-        image: 'http://image',
-        address: 'An address in a place',
-        description: 'A nice meetup'
-    },
-    {
-        id: 'm2',
-        title: 'Meetup 2',
-        image: 'http://image',
-        address: 'An address in a place',
-        description: 'A nice meetup'
-    },
-    {
-        id: 'm3',
-        title: 'Meetup 3',
-        image: 'http://image',
-        address: 'An address in a place',
-        description: 'A nice meetup'
-    },
-]
+  {
+    id: "m1",
+    title: "Meetup 1",
+    image: "http://image",
+    address: "An address in a place",
+    description: "A nice meetup",
+  },
+  {
+    id: "m2",
+    title: "Meetup 2",
+    image: "http://image",
+    address: "An address in a place",
+    description: "A nice meetup",
+  },
+  {
+    id: "m3",
+    title: "Meetup 3",
+    image: "http://image",
+    address: "An address in a place",
+    description: "A nice meetup",
+  },
+];
 
-function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS}/>
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
+
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+  
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
+
+//export async function getStaticProps() {
+//return {
+// props: {
+//   meetups: DUMMY_MEETUPS,
+// },
+// revalidate: 10
+// };
+//}
 
 export default HomePage;
