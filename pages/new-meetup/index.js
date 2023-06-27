68b5eac5065696a57da1d6d4b3a46e77b6f8a931
VivienIngrams <vivingrams@gmail.com>
2023-06-27 12:12:02 +0100

    Sending http request to mongodb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,23 @@
+import { useRouter } from 'next/router';
+
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-    function addMeetupHandler(newMeetupData) {
+
+    const router = useRouter();
+
+    async function addMeetupHandler(newMeetupData) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(newMeetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await  response.json();
+
+        router.push('/');
 
     }
 return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
